69be82c137fb9cad4a033ba682f1c4e8e6a7e501
Moringa <alinoorshukri8@gmail.com>
2022-06-10 09:03:11 +0300

    commit tree f5f25a5cf74037fe0e9e56ab06b6f95b19e7c7d2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array){
+    function test(w){
+        return (w.result === 'W')
+    }
+    if (array.find(test)){
+     return array.find(test).year
+    }
+}
